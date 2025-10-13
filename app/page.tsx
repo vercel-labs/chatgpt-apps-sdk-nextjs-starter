0827c51fdda80d12e8b1a5b654f3a0fc97b838d4
Allen Zhou <46854522+allenzhou101@users.noreply.github.com>
2025-10-13 16:02:18 -0700

    remove min height

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
 
   return (
     <div 
-      className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20"
+      className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center p-8 pb-20 gap-16 sm:p-20"
       style={{
         maxHeight,
         height: displayMode === "fullscreen" ? maxHeight : undefined,
